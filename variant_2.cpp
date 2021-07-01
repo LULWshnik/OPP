a92d0c32b9d7cd4f5359f8bbf63f78b0bff23ad4
LULWshnik <54348523+LULWshnik@users.noreply.github.com>
2021-07-01 19:56:23 +0700

    variant2 1.0

--- variant_2.cpp
+++ variant_2.cpp
@@ -1,139 +1,94 @@
-#include "mpi.h"
+#include "omp.h"
 #include <iostream>
 
-#define e 0.00000001
-#define MATRIX_SIZE 10000
-#define t 0.01
-#define NORM 0
+#define e 0.000001
+#define t 0.001
+#define MATRIX_SIZE 5500
 using namespace std;
 
-void Counting(double* A, double* B, double* X, int* rows_at_process, double* rightResult, int* shift) {
-	int size, rank;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	int thisRank = (rank + size) % size;
-	for (int i = 0; i < size; ++i) {
-		for (int j = 0; j < rows_at_process[rank]; ++j) {
-			for (int k = shift[thisRank]; k < shift[thisRank] + rows_at_process[thisRank]; ++k) {
-				rightResult[j] += A[k + j * MATRIX_SIZE] * X[k - shift[thisRank]];
-			}
+void right(double* A, double* b, double* xn, double* rightResult) {
+#pragma omp for
+	for (int j = 0; j < MATRIX_SIZE; ++j) {
+		rightResult[j] = 0;
+		for (int i = 0; i < MATRIX_SIZE; ++i) {
+			rightResult[j] += A[i + j * MATRIX_SIZE] * xn[i];
 		}
-		thisRank = (rank - i + size) % size;
-
-		MPI_Sendrecv_replace(X, rows_at_process[0], MPI_DOUBLE, (rank + 1) % size, 0, (rank - 1 + size) % size, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-	}
-	for (int j = 0; j < rows_at_process[rank]; ++j) {
-		rightResult[j] -= B[j];
+		rightResult[j] -= b[j];
 	}
 	return;
 }
 
-bool flag(double* rightResult, double normB, int* rows_at_process, int rank) {
-	double norm = 0;
-	for (int i = 0; i < rows_at_process[rank]; ++i) {
-		norm += rightResult[i] * rightResult[i];
+double sqrTop = 0;
+
+bool flag(double* rightResult, double* b, double sqrB) {
+	sqrTop = 0;
+#pragma omp for reduction(+:sqrTop) 
+	for (int i = 0; i < MATRIX_SIZE; ++i) {
+		sqrTop += rightResult[i] * rightResult[i];
 	}
-	double temp = 0;
-	double tempB = 0;
-	MPI_Allreduce(&norm, &temp, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-	MPI_Allreduce(&normB, &tempB, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-	temp = sqrt(temp / tempB);
-	return temp > e;
+
+	return sqrt(sqrTop / sqrB) > e;
 }
 
-int main(int argc, char* argv[])
-{
-
-	int size, rank;
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-	double start;
-	double* A = NULL;
-	double* X = new double[MATRIX_SIZE];
-	double* B = new double[MATRIX_SIZE];
-	double norm;
-	double normB = 0.0;
-
-	int* rows_at_process = new int[size];//массив кол-ва строк по процессам
-
-	for (int i = 0; i < size; ++i) {
-		rows_at_process[i] = (MATRIX_SIZE / size) + ((i < MATRIX_SIZE % size) ? (1) : (0));//распределяем строки по процессам 
-	}
-
-	int* shift = new int[size];
-	fill(shift, shift + size, 0);
-	for (int i = 1; i < size; ++i) {
-		shift[i] = shift[i - 1] + rows_at_process[i - 1];
-	}
-
-	A = new double[rows_at_process[rank] * MATRIX_SIZE]; //выделяем память под часть матрицы для процесса
-
-	double* Xn = new double[rows_at_process[rank]]; //результат уможения матрицы на вектор для конкретного процесса
-
-	for (int i = 0; i < rows_at_process[rank]; ++i) {
+int main(int argc, char** argv) {
+	double* Xn = new double[MATRIX_SIZE];
+	double* A = new double[MATRIX_SIZE * MATRIX_SIZE];
+	for (int i = 0; i < MATRIX_SIZE; ++i) {
 		for (int j = 0; j < MATRIX_SIZE; ++j) {
-			A[i * MATRIX_SIZE + j] = (shift[rank] + 1 == j) + 1;
+			A[j + i * MATRIX_SIZE] = 1.0;
+			if (i == j) {
+				++A[j + i * MATRIX_SIZE];
+			}
 		}
+	}
+
+	double* B = new double[MATRIX_SIZE];
+	for (int i = 0; i < MATRIX_SIZE; ++i) {
 		B[i] = MATRIX_SIZE + 1;
-		Xn[i] = 0;
-		normB += B[i] * B[i];
 	}
 
-
-	
-	for (int i = 0; i < MATRIX_SIZE; ++i) { 
-
-		X[i] = 0.0;
+	double* rightResult = (double*)calloc(MATRIX_SIZE, sizeof(double));
+	for (int i = 0; i < MATRIX_SIZE; ++i) {
+		rightResult[i] = (-1) * B[i];
 	}
 
-	if (rank) {
-
-		MPI_Send(&normB, 1, MPI_DOUBLE, 0, NORM, MPI_COMM_WORLD);
+	double sqrB = 0;
+	for (int i = 0; i < MATRIX_SIZE; ++i) {
+		sqrB += B[i] * B[i];
 	}
-	else{
 
-		start = MPI_Wtime();
-
-		for (int i = 1; i < size; ++i) {
-			double tmp;
-			MPI_Recv(&tmp, 1, MPI_DOUBLE, i, NORM, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			normB += tmp;
+	double startTime = omp_get_wtime();
+#pragma omp parallel num_threads(1)
+	{
+		int exitCond = 0;
+		while ((flag(rightResult, B, sqrB)) && (exitCond < 10000))
+		{
+#pragma omp for
+			for (int i = 0; i < MATRIX_SIZE; ++i) {
+				Xn[i] = Xn[i] - t * rightResult[i];
+			}
+			right(A, B, Xn, rightResult);
+			++exitCond;
 		}
-
-		normB = sqrt(normB);
 	}
-	
-	double* rightResult = new double[rows_at_process[rank]];
-	
-	Counting(A, B, X, rows_at_process, rightResult, shift);
+	double endTime = omp_get_wtime();
 
-	while (flag(rightResult, normB, rows_at_process, rank)) {
-		norm = 0.0;
-		for (int i = 0; i < rows_at_process[rank]; ++i) {
-			X[i] -= t * rightResult[i];
+
+	double temp = fabs(Xn[0]);
+	for (int i = 0; i < MATRIX_SIZE; ++i) {
+		if (temp < fabs(Xn[i])) {
+			temp = fabs(Xn[i]);
 		}
-
-		Counting(A, B, X, rows_at_process, rightResult, shift);
-
 	}
 
-	free(shift);
 	delete[] A;
 	delete[] B;
-	delete[] X;
-	delete[] rightResult;
-	delete[] rows_at_process;
 	delete[] Xn;
-
-	if (!rank) {
-		double end = MPI_Wtime();
-		cout << "Execution time: " << end - start << "\n";
+	delete[] rightResult;
+	cout << "Execution time: " << endTime - startTime << "\n";
+	if (temp > 1 + e) {
+		cout << "Problem: " << temp << "\n";
+		return 1;
 	}
-	
-	MPI_Finalize();
-
 	return 0;
 }
